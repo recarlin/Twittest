c20c36d1a5ed5e11d6b1d9d30680816e7ee8531c
recarlin <recarlin@live.com>
2013-04-25 22:51:26 -0400

    Change of plans! I'm switching my app to use a weather API

--- src/com/recarlin/twittest/MainActivity.java
+++ src/com/recarlin/twittest/MainActivity.java
@@ -8,12 +8,16 @@ package com.recarlin.twittest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import TweetBuilder.AddNReadTweets;
 import TweetBuilder.SendRequest;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -22,13 +26,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	Boolean connected = false;
 	EditText nameEntry;
 	EditText tweetEntry;
 	LinearLayout lay;
+	ScrollView scroll;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,42 +45,47 @@ public class MainActivity extends Activity {
 		lay.setOrientation(LinearLayout.VERTICAL);
 		LinearLayout.LayoutParams par = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
 		lay.setLayoutParams(par);
-		
 		//Sets up a title text view.
 		TextView title = new TextView(this);
-		title.setText("Twitter Feed");
+		title.setText("3-Day Forecast");
 		title.setTextSize(40);
 		title.setGravity(Gravity.CENTER);
 		
 		LinearLayout.LayoutParams par2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-		
-		//Sets up a button, with click listener.
+		//Button that will send the request, as long as you are connected to the Internet. If you aren't, you get an error.
 		Button saveTweet = new Button(this);
-		saveTweet.setText("Get Timeline");
-		//This listener will generate a new Tweet based on what is in the EditText fields.
-		//At the end, it adds the new Tweet to the main view.
+		saveTweet.setText("Get Forecast");
 		saveTweet.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String username = tweetEntry.getText().toString();
-				getTimelineURL(username);
+				connected = SendRequest.getConnected(MainActivity.this);
+				if(connected) {
+					String username = tweetEntry.getText().toString();
+					getTimelineURL(username);
+				} else {
+					Log.i("CONNECTION", "You are not connected to the Internet!");
+				}
 			}
 		});
-		
-		//Sets up a EditText and TextView for users to type their Tweet.
+		//Sets up a EditText and TextView for users to type their zip code.
 		LinearLayout tweet = new LinearLayout(this);
 		tweet.setOrientation(LinearLayout.HORIZONTAL);
 		tweet.setLayoutParams(par2);
 		par2 = new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1.0f);
 		tweetEntry = new EditText(this);
-		tweetEntry.setHint("Twitter Username");
+		tweetEntry.setInputType(InputType.TYPE_CLASS_NUMBER);
+		tweetEntry.setHint("Enter Zip Code");
 		tweetEntry.setLayoutParams(par2);
 		tweet.addView(tweetEntry);
 		tweet.addView(saveTweet);
 		
+		scroll = new ScrollView(this);
+		scroll.setLayoutParams(par);
+		
 		//Here we add all of the views to the main view.
 		lay.addView(title);
 		lay.addView(tweet);
+		lay.addView(scroll);
 		setContentView(lay);
 	}
 
@@ -83,17 +95,17 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+	//This builds a URL for the request, based on what you pass into it from the EditText, and executes the getTimeline instance.
 	private void getTimelineURL(String username) {
 		try {
-			URL userTimelineURL = new URL("https://api.twitter.com/1.1/statuses/user_timeline.json?" + username + "=twitterapi&count=20");
+			URL userTimelineURL = new URL("http://api.wunderground.com/api/137996d2b3a91dcf/forecast/q/" + username + ".json");
 			getTimeline gtl = new getTimeline();
 			gtl.execute(userTimelineURL);
 		} catch(MalformedURLException e) {
 			Log.e("MALFORMED URL", "URL is incorrect!");
 		}
 	}
-	
+	//Performs the URL request, reads the data, puts it into a TextView, and adds it to the main view.
 	private class getTimeline extends AsyncTask<URL, Void, String>{
 		@Override
 		protected String doInBackground(URL...urls) {
@@ -106,7 +118,16 @@ public class MainActivity extends Activity {
 		
 		@Override
 		protected void onPostExecute(String result) {
-			Log.i("URL RESPONSE", result);
+			try {
+				JSONObject resultJSON = new JSONObject(result);
+				JSONArray results = resultJSON.getJSONObject("forecast").getJSONObject("txt_forecast").getJSONArray("forecastday");
+				String post = AddNReadTweets.ReadDefaults(results);
+				TextView resultsTextView = AddNReadTweets.NewTweetView(post, MainActivity.this);
+				scroll.addView(resultsTextView);
+			} catch(JSONException e) {
+				Log.e("JSON ERROR", "Your JSON is incorrect!");
+			}
+			
 		}
 	}
 }
